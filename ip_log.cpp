b49fa6f1fdbd6d625355cedd7282bddf489786b3
jameshloving <jameshloving@gmail.com>
2016-05-29 19:13:24 -0400

    Fix indentation

--- ip_log.cpp
+++ ip_log.cpp
@@ -15,34 +15,34 @@ class ip_log
 {
 protected:
   bloom_parameters parameters;
-	bloom_filter filter;
+  bloom_filter filter;
 
 public:
   ip_log(int count, float probability)
-	{
-		if (count > 0)
-		{
-			parameters.projected_element_count = count;
-		}
-		else
-		{
-			throw std::invalid_argument("Invalid ip_log.projected_element_count: " + std::to_string(count));
-		}
+  {
+    if (count > 0)
+    {
+      parameters.projected_element_count = count;
+    }
+    else
+    {
+      throw std::invalid_argument("Invalid ip_log.projected_element_count: " + std::to_string(count));
+    }
 
-		if (probability > -1.0 && probability < 1.0)
-		{
-			parameters.false_positive_probability = probability;
-		}
-		else
-		{
-			throw std::invalid_argument("Invalid ip_log.false_positive_probability: " + std::to_string(probability));
-		}
+    if (probability > -1.0 && probability < 1.0)
+    {
+      parameters.false_positive_probability = probability;
+    }
+    else
+    {
+      throw std::invalid_argument("Invalid ip_log.false_positive_probability: " + std::to_string(probability));
+    }
 
-		parameters.compute_optimal_parameters();
+    parameters.compute_optimal_parameters();
 
-		bloom_filter filter_temp(parameters);
-		filter = filter_temp;
-	}
+    bloom_filter filter_temp(parameters);
+    filter = filter_temp;
+  }
 
 };
 
@@ -53,12 +53,12 @@ class ipv4_log
 public:
   ipv4_log(int count, float probability)
   : ip_log(count, probability) {}
-
+  
   // TODO: IPv4 data type
-	void add_connection(uint32_t address, uint16_t port)
-	{
-		filter.insert(address);
-	}
+  void add_connection(uint32_t address, uint16_t port)
+  {
+    filter.insert(address);
+  }
 };
 
 // TODO: subclass for IPv6 timeslot log
@@ -68,9 +68,9 @@ public:
 // TODO: remove, this is for testing only
 int main()
 {
-	ipv4_log log(10000, 0.001);
+  ipv4_log log(10000, 0.001);
 
-	log.add_connection(3, 10);
+  log.add_connection(3, 10);
 
-	return 0;
+  return 0;
 }
