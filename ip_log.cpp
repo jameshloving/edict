0c8b422688a718336bf25967298ff8ff1d03e0eb
jameshloving <jameshloving@gmail.com>
2016-06-05 18:13:39 -0400

    Support adding to ip_log w/ proper sublog length

--- ip_log.cpp
+++ ip_log.cpp
@@ -3,6 +3,7 @@
   James Loving
 **/
 
+#include <chrono>
 #include <exception>
 #include <iostream>
 #include <queue>
@@ -11,12 +12,17 @@
 
 #include "libs/bloom/bloom_filter.hpp"
 
+const int SUBLOG_LENGTH = 3600; // sublog length in seconds
+const int DEFAULT_COUNT = 1000000; // default max capacity for Bloom filter
+const float DEFAULT_PROBABILITY = 0.001; // default probability for Bloom filter false positive
+
 // parent class for IPv4 and IPv6 sublogs
 class ip_sublog_parent
 {
 protected:
   bloom_parameters parameters;
   bloom_filter filter;
+  std::chrono::steady_clock::time_point creation_time;
 
 public:
   ip_sublog_parent(int count, float probability)
@@ -27,7 +33,7 @@ public:
     }
     else
     {
-      throw std::invalid_argument("Invalid ip_log.projected_element_count: "
+      throw std::invalid_argument("Invalid ip_sublog_parent.projected_element_count: "
                                   + std::to_string(count));
     }
 
@@ -37,7 +43,7 @@ public:
     }
     else
     {
-      throw std::invalid_argument("Invalid ip_log.false_positive_probability: "
+      throw std::invalid_argument("Invalid ip_sublog_parent.false_positive_probability: "
                                   + std::to_string(probability));
     }
 
@@ -45,6 +51,8 @@ public:
 
     bloom_filter filter_temp(parameters);
     filter = filter_temp;
+    
+    creation_time = std::chrono::steady_clock::now();
   }
 };
 
@@ -66,7 +74,16 @@ struct ipv6_source
 class ipv4_sublog
 : protected ip_sublog_parent
 {
+private:
+  bool valid_port(uint16_t port)
+  {
+    return (port >= 0 && port < 65536);
+  }
+  
 public:
+  ipv4_sublog()
+  : ip_sublog_parent(DEFAULT_COUNT, DEFAULT_PROBABILITY) {}
+
   ipv4_sublog(int count, float probability)
   : ip_sublog_parent(count, probability) {}
 
@@ -74,8 +91,9 @@ public:
   {
     ipv4_source source;
     source.mac = mac_address;
+    // TODO: MAC address validation
     
-    if (port >= 0 && port < 65536)
+    if (valid_port(port))
     {
       source.port = port;
     }
@@ -85,15 +103,24 @@ public:
                                   + std::to_string(port));
     }
 
-    filter.insert(source);
+    if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now()
+                                                         - creation_time).count() < SUBLOG_LENGTH)
+    {
+      filter.insert(source);
+    }
+    else
+    {
+      throw std::out_of_range("Beyond ipv4 sublog length");
+    }
   }
 
   bool has_connection(std::string mac_address, uint16_t port)
   {
     ipv4_source source;
     source.mac = mac_address;
+    // TODO: MAC address validation
 
-    if (port >= 0 && port < 65536)
+    if (valid_port(port))
     {
       source.port = port;
     }
@@ -116,6 +143,29 @@ private:
   // TODO: IPv6 queue
 
 public:
+  void add_ipv4_connection(std::string mac_address, uint16_t port)
+  {
+    if (ipv4_log.size() == 0)
+    { 
+      ipv4_sublog sublog;
+      ipv4_log.push(sublog);
+    }
+
+    try
+    {
+      ipv4_log.back().add_connection(mac_address, port);
+    }
+    catch (const std::out_of_range& e)
+    {
+      ipv4_sublog sublog;
+      ipv4_log.push(sublog);
+      ipv4_log.back().add_connection(mac_address, port);
+    }
+     // try to add to queue.first sublog
+      // catch exception
+      // push new sublog, 
+      // and add to new queue.first sbblog
+  }
   // TODO: add new sublog to log (done at end of timeslot)
   // TODO: delete oldest sublog (done at end of timeslot) IFF out of space
   // TODO: add connection to current sublog
@@ -127,7 +177,7 @@ public:
 // TODO: remove, this is for testing only
 int main()
 {
-  ipv4_sublog log(10000, 0.001);
+  ipv4_sublog log(1000000, 0.001);
   log.add_connection("aa-bb-cc-dd-ee-ff", 10);
   std::cout << log.has_connection("aa-bb-cc-dd-ee-ff",10) << "\n";
   std::cout << sizeof(log) << " bytes\n";
