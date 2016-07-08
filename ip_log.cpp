5e80a371476ee24c163d96532d046f1c210b76f3
jameshloving <jameshloving@gmail.com>
2016-07-08 18:28:19 -0400

    Push forward validity checks, remove debugging lines

--- ip_log.cpp
+++ ip_log.cpp
@@ -27,17 +27,6 @@ protected:
   bloom_filter *filter;
   time_t creation_time;
 
-  // TODO: fix these functions
-  bool valid_mac(std::string mac) const
-  {
-    return true;
-  }
-
-  bool valid_ipv6(std::string ipv6) const
-  {
-    return true;
-  }
-
 public:
   ip_sublog()
   {
@@ -56,15 +45,20 @@ public:
     return creation_time;
   }
 
+  // TODO: fix these functions
+  bool valid_mac(std::string mac) const
+  {
+    return true;
+  }
+
+  bool valid_ipv6(std::string ipv6) const
+  {
+    return true;
+  }
+
   void add_ipv4(std::string mac_address,
                 uint16_t port)
   {
-    if (!valid_mac(mac_address))
-    {
-      throw std::invalid_argument("Invalid ip_sublog.add_ipv4(mac_address): "
-                                  + mac_address);
-    }
-
     if ((time(nullptr) - creation_time) < SUBLOG_LENGTH)
     {
       std::cout << "    Inserted @ " << time(nullptr) << "\n";
@@ -79,24 +73,12 @@ public:
   bool has_ipv4(std::string mac_address,
                 uint16_t port) const
   {
-    if (!valid_mac(mac_address))
-    {
-      throw std::invalid_argument("Invalid ip_sublog.has_ipv4(mac_address): "
-                                  + mac_address);
-    }
-
     return filter->contains(mac_address + std::to_string(port));
   }
 
   void add_ipv6(std::string mac_address,
                 std::string ipv6_address)
   {
-    if (!valid_mac(mac_address))
-    {
-      throw std::invalid_argument("Invalid ip_sublog.add_ipv6(mac_address): "
-                                  + mac_address);
-    }
-
     if ((time(nullptr) - creation_time) < SUBLOG_LENGTH)
     {
       filter->insert(mac_address + ipv6_address);
@@ -110,18 +92,6 @@ public:
   bool has_ipv6(std::string mac_address,
                 std::string ipv6_address) const
   {
-    if (!valid_mac(mac_address))
-    {
-      throw std::invalid_argument("Invalid ip_sublog.has_ipv6(mac_address): "
-                                  + mac_address);
-    }
-
-    if (!valid_ipv6(ipv6_address))
-    {
-      throw std::invalid_argument("Invalid ip_sublog.has_ipv6(ipv6_address): "
-                                  + ipv6_address);
-    }
-
     return filter->contains(mac_address + ipv6_address);
   }
 };
@@ -142,6 +112,12 @@ public:
   void add_ipv4_connection(std::string mac_address,
                            uint16_t port)
   {
+    if (!valid_mac(mac_address))
+    {
+      throw std::invalid_argument("Invalid ip_log.add_ipv4_connection(mac_address): "
+                                  + mac_address);
+    }
+
     if (log.size() == 0)
     {
       ip_sublog *sublog = new ip_sublog;
@@ -168,24 +144,90 @@ public:
                            uint16_t port,
                            time_t timestamp) const
   {
-    int i = 0;
+    if (!valid_mac(mac_address))
+    {
+      throw std::invalid_argument("Invalid ip_log.has_ipv4_connection(mac_address): "
+                                  + mac_address);
+    }
+
     for(auto it = log.cbegin(); it != log.cend(); it++)
     {
-      std::cout << "    sublog: " << i++ << "\n";
-      std::cout << "      creation_time: " << it->get_creation_time() << "\n";
-      std::cout << "    timestamp: " << timestamp << "\n";
-      std::cout << "    front().has_connection(): " << log.front().has_ipv4(mac_address, port) << "\n";
-      std::cout << "    back().has_connection(): " << log.back().has_ipv4(mac_address, port) << "\n";
       // compare timestamp and creation times to get correct log
       if (timestamp >= it->get_creation_time() &&
           timestamp - it->get_creation_time() < SUBLOG_LENGTH)
       {
-       // check for MAC+port combination in log
+        // check for MAC+port combination in log
         return it->has_ipv4(mac_address, port);
       }
     }
+
     throw std::out_of_range("Invalid timestamp - no ipv4 log covering the timestamp's period");
   }
 
+  void add_ipv6_connection(std::string mac_address,
+                           std::string ipv6_address)
+  {
+    if (!valid_mac(mac_address))
+    {
+      throw std::invalid_argument("Invalid ip_log.add_ipv6_connection(mac_address): "
+                                  + mac_address);
+    }
 
+    if (!valid_ipv6(ipv6_address))
+    {
+      throw std::invalid_argument("Invalid ip_log.add_ipv6_connection(ipv6_address): "
+                                  + ipv6_address);
+    }
+
+    if (log.size() == 0)
+    {
+      ip_sublog *sublog = new ip_sublog;
+      log.push_back(*sublog);
+    }
+
+    try
+    {
+      log.back().add_ipv4(mac_address, port);
+    }
+    catch (const std::out_of_range& e)
+    {
+      ip_sublog *sublog = new ip_sublog;
+      log.push_back(*sublog);
+      log.back().add_ipv4(mac_address, port);
+      if (log.size() == capacity)
+      {
+        log.pop_front();
+      }
+    }
+  }
+
+  bool has_ipv6_connection(std::string mac_address,
+                           std::string ipv6_address,
+                           time_t timestamp) const
+  {
+    if (!valid_mac(mac_address))
+    {
+      throw std::invalid_argument("Invalid ip_log.has_ipv6_connection(mac_address): "
+                                  + mac_address);
+    }
+
+    if (!valid_ipv6(ipv6_address))
+    {
+      throw std::invalid_argument("Invalid ip_log.has_ipv6_connection(ipv6_address): "
+                                  + ipv6_address);
+    }
+
+    for(auto it = log.cbegin(); it != log.cend(); it++)
+    {
+      // compare timestamp and creation times to get correct log
+      if (timestamp >= it->get_creation_time() &&
+          timestamp - it->get_creation_time() < SUBLOG_LENGTH)
+      {
+        // check for MAC+port combination in log
+        return it->has_ipv4(mac_address, port);
+      }
+    }
+
+    throw std::out_of_range("Invalid timestamp - no ipv4 log covering the timestamp's period");
+  }
 };
