4883fd95daa16a3e37e7b22b783de6044ec9273d
jameshloving <jameshloving@gmail.com>
2016-09-15 20:20:12 -0400

    Update edict.c to MAC logging frame

--- events/edict.c
+++ events/edict.c
@@ -1,79 +1,80 @@
+/* ulog_test, $Revision: 1.4 $
+ *
+ * small testing program for libipulog, part of the netfilter ULOG target
+ * for the linux 2.4 netfilter subsystem.
+ *
+ * (C) 2000 by Harald Welte <laforge@gnumonks.org>
+ *
+ * this code is released under the terms of GNU GPL
+ *
+ * $Id: ulog_test.c 286 2002-06-13 12:56:53Z laforge $
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <arpa/inet.h>
+#include <string.h>
+#include <libnetfilter_log/libipulog.h>
 
-#include <libmnl/libmnl.h>
-#include <libnetfilter_conntrack/libnetfilter_conntrack.h>
+#define MYBUFSIZ 2048
 
-static int data_cb(const struct nlmsghdr *nlh, void *data)
+/* prints some logging about a single packet */
+void handle_packet(ulog_packet_msg_t *pkt)
 {
-	struct nf_conntrack *ct;
-	uint32_t type = NFCT_T_UNKNOWN;
-	char buf[4096];
-
-	switch(nlh->nlmsg_type & 0xFF) {
-	case IPCTNL_MSG_CT_NEW:
-		if (nlh->nlmsg_flags & (NLM_F_CREATE|NLM_F_EXCL))
-			type = NFCT_T_NEW;
-		else
-			type = NFCT_T_UPDATE;
-		break;
-	case IPCTNL_MSG_CT_DELETE:
-		type = NFCT_T_DESTROY;
-		break;
+	unsigned char *p;
+	int i;
+	
+	if (pkt->mac_len)
+	{
+		printf("mac=");
+		p = pkt->mac;
+		for (i = 0; i < pkt->mac_len; i++, p++)
+			printf("%02x%c", *p, i==pkt->mac_len-1 ? ' ':':');
 	}
+	printf("\n");
 
-	ct = nfct_new();
-	if (ct == NULL)
-		return MNL_CB_OK;
-
-	nfct_nlmsg_parse(nlh, ct);
-
-	nfct_snprintf(buf, sizeof(buf), ct,
-			type, NFCT_O_DEFAULT, 0);
-	printf("%s\n", buf);
-
-	nfct_destroy(ct);
-
-	return MNL_CB_OK;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	struct mnl_socket *nl;
-	char buf[MNL_SOCKET_BUFFER_SIZE];
-	int ret;
+	struct ipulog_handle *h;
+	unsigned char* buf;
+	int len;
+	ulog_packet_msg_t *upkt;
+	int i;
 
-	nl = mnl_socket_open(NETLINK_NETFILTER);
-	if (nl == NULL) {
-		perror("mnl_socket_open");
-		exit(EXIT_FAILURE);
+	if (argc != 4) {
+		fprintf(stderr, "Usage: %s count group \n", argv[0]);
+		exit(2);
 	}
 
-	if (mnl_socket_bind(nl, NF_NETLINK_CONNTRACK_NEW |
-				NF_NETLINK_CONNTRACK_UPDATE |
-				NF_NETLINK_CONNTRACK_DESTROY,
-				MNL_SOCKET_AUTOPID) < 0) {
-		perror("mnl_socket_bind");
-		exit(EXIT_FAILURE);
+	/* allocate a receive buffer */
+	buf = malloc(MYBUFSIZ);
+	if (!buf)
+		exit(1);
+	
+	/* create ipulog handle */
+	h = ipulog_create_handle(ipulog_group2gmask(atoi(argv[2])), 65535);
+	if (!h)
+	{
+		/* if some error occurrs, print it to stderr */
+		ipulog_perror(NULL);
+		exit(1);
 	}
 
-	while (1) {
-		ret = mnl_socket_recvfrom(nl, buf, sizeof(buf));
-		if (ret == -1) {
-			perror("mnl_socket_recvfrom");
-			exit(EXIT_FAILURE);
+	/* loop receiving packets and handling them over to handle_packet */
+	for (i = 0; i < atoi(argv[1]); i++) {
+		len = ipulog_read(h, buf, MYBUFSIZ, 1);
+		if (len <= 0) {
+			ipulog_perror("ulog_test: short read");
+			exit(1);
 		}
-
-		ret = mnl_cb_run(buf, ret, 0, 0, data_cb, NULL);
-		if (ret == -1) {
-			perror("mnl_cb_run");
-			exit(EXIT_FAILURE);
+		printf("%d bytes received\n", len);
+		while ((upkt = ipulog_get_packet(h, buf, len)) != NULL) {
+			handle_packet(upkt);
 		}
 	}
-
-	mnl_socket_close(nl);
-
+	
+	/* just to give it a cleaner look */
+	ipulog_destroy_handle(h);
 	return 0;
 }
