1e8f238372005792b9e7ebc1b213334eba34a213
root <root@edict-dev.csail.mit.edu>
2016-10-27 15:41:18 -0400

    Working outline, needs check_ipv4/ipv6

--- query/query.cpp
+++ query/query.cpp
@@ -1,11 +1,14 @@
 #include <arpa/inet.h>
-#include <unordered_map>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <fcntl.h>
 #include <iostream>
+#include <netinet/in.h>
 #include <sstream>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <time.h>
+#include <unistd.h>
+#include <unordered_map>
 
 #include "../libs/ip_log/ip_log.cpp"
 
@@ -15,35 +18,103 @@ struct device_log_entry
 	time_t first_seen;		
 };
 
-// TODO: put these into shared memory
-//std::unordered_map<std::string, struct device_log_entry> device_log;
+struct region
+{
+    ip_log conn_log;
+};
+struct region *shm;
 
-//ip_log conn_log;
+bool check_ipv4(time_t timestamp, uint16_t source_port)
+{
+    return true;
+}
+
+bool check_ipv6(time_t timestamp, std::string source_ipv6)
+{
+    return true;
+}
 
 int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "Usage: " << argv[0] << " <timestamp> <\"-4\" or \"-6\"> <source port OR IPv6 source address>\n";
+		std::cout << "Usage: " << argv[0] << " <timestamp> <\"v4\" or \"v6\"> <source port OR IPv6 source address>\n";
 		return 0;
 	}
 
+    time_t timestamp;
+    int fd_shm;
+
 	// TODO: validate timestamp
-	// TODO: convert timestamp to time_t
+    if (true)
+    {
+        timestamp = static_cast<time_t>(atoi(argv[1]));
+    }	
+    else
+    {
+        std::cout << "Invalid timestamp.\n";
+        return 0;
+    }
 
-	// TODO: determine source port vs. IPv6 address
-	// TODO: IF source port, validate source port and assign
-	// TODO: IF source IPv6, validate address and assign
+    // Create shared memory object and set its size
+    fd_shm = shm_open("/myregion", O_RDONLY, S_IRUSR | S_IWUSR);
+    if (fd_shm == -1)
+    {
+        std::cerr << "can't shm_open /myregion: " << errno << "\n";
+        exit(1);
+    }
 
-	// TODO: locate memory segment
+    /*
+    if (ftruncate(fd_shm, sizeof(struct region)) == -1)
+    {
+        std::cerr << "can't ftruncate fd_shm: " << errno << "\n";
+        exit(1);
+    }
+    */
 
-	// TODO: attach mem segment to data space
+    shm = static_cast<struct region *>(mmap(NULL, sizeof(struct region),
+                                            PROT_READ, MAP_SHARED,
+                                            fd_shm, 0));
 
-	// TODO: read what edict.cpp put into memory
+    shm = new struct region();
 
-	// TODO: query the log
+    if (shm == MAP_FAILED)
+    {
+        fprintf(stderr, "can't mmap shm\n");
+        exit(1);
+    }
 
-	// TODO: output the result
+    if (strcmp(argv[2], "v4") == 0)
+    {
+        if (true) // TODO: validate IPv4 source port
+        {
+            uint16_t source_port = static_cast<uint16_t>(atoi(argv[3]));
+            std::cout << check_ipv4(timestamp, source_port) << "\n";
+        }
+        else
+        {
+            std::cout << "Invalid source port.\n";
+            return 0;
+        }
+    }
+    else if (strcmp(argv[2], "v6") == 0)
+    {
+        if (true) // TODO: validate IPv6 source address
+        {
+            std::string source_ipv6(argv[3]);
+            std::cout << check_ipv6(timestamp, source_ipv6) << "\n";
+        }
+        else
+        {
+            std::cout << "Invalid source IPv6 address.\n";
+            return 0;
+        }
+    }
+    else
+    {
+        std::cout << "Invalid protocol.\n";
+		return 0;
+    }
 
 	return 0;
 }
