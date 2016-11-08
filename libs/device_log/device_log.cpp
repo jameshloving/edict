2263ff6a376f3a5c5a0931b571a4581610bfff93
jameshloving <jameshloving@gmail.com>
2016-11-08 15:54:39 -0500

    Add device_log.count()

--- libs/device_log/device_log.cpp
+++ libs/device_log/device_log.cpp
@@ -11,6 +11,7 @@
 #include <string>         // string class
 #include <time.h>         // time(), etc.
 #include <map>
+#include <unordered_set>
 
 const char DEVICE_LOG_FILE[] = "../stor/device_log.txt";
 
@@ -23,6 +24,7 @@ struct device_log_entry
 class device_log
 {
 protected:
+    std::unordered_set<std::string> macs;
 
 public:
     device_log()
@@ -32,6 +34,8 @@ public:
     void add_device(std::string mac_address,
                     std::string make_model)
     {
+        macs.insert(mac_address);
+
         std::ofstream outfile;
         outfile.open(DEVICE_LOG_FILE, std::ios::out | std::ios::app);
 
@@ -46,6 +50,11 @@ public:
 
         outfile << "\n";
         outfile.close();
+    }
+
+    unsigned int count(std::string mac_address)
+    {
+        return macs.count(mac_address);
     }
 
     std::map<std::string, struct device_log_entry> read_devices()
