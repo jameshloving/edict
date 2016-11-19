09211ae7eda3d1234c1891ce76832b82b0b176cd
jameshloving <jameshloving@gmail.com>
2016-11-19 11:52:45 -0500

    Add tcp_client wrapper, add_ipv4

--- libs/conn_log/conn_log.cpp
+++ libs/conn_log/conn_log.cpp
@@ -4,45 +4,64 @@
 */
 
 #include <arpa/inet.h>    // convert IPv6 strings <-> sockaddr_in6
+#include <cstring>
 #include <exception>      // exception handling
 #include <iostream>       // output
 #include <memory>         // unique_ptr
+#include <netdb.h>
 #include <netinet/in.h>   // sockaddr_in6 IPv6 struct
 #include <queue>          // basis for log-of-sublogs
 #include <regex>          // for MAC and IP address validation
+#include <sys/socket.h>   // for network sockets
+#include <sys/types.h>
 #include <string>         // string class
 #include <stdint.h>       // int vars of atypical size (16b, 32b)
 #include <time.h>         // time(), etc.
+#include <unistd.h>
 
+#include "tcp_client.cpp"
 #include "../bloom/bloom_filter.hpp"   // Bloom filter, by Arash Partow
 
 const int SUBLOG_LENGTH = 3600;            // sublog length in seconds
 const int SUBLOG_FUZZINESS = 10;           // number of seconds of fuzziness in checking sublogs
 const int DEFAULT_COUNT = 2000000;         // default max capacity for Bloom filter
 const float DEFAULT_PROBABILITY = 0.001;   // default probability for Bloom filter false positive
-const unsigned int DEFAULT_CAPACITY = 168; // default number of timeslots' sublogs conn_log stores
 
 class conn_log
 {
     private:
+        tcp_client c;
+
         // TODO: fix these functions
         bool valid_mac(std::string mac) const
         {
-            std::regex pattern("^(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}|(?:[0-9a-fA-F]{2}-){5}[0-9a-fA-F]{2}|(?:[0-9a-fA-F]{2}){5}[0-9a-fA-F]{2}$");
+            std::regex pattern("^([0-9a-F]{1,2}[:-]){5}([0-9a-F]{1,2})$", std::regex_constants::basic);
+            return true;
             return std::regex_match(mac, pattern);
         }
 
         bool valid_ipv6(std::string ipv6) const
         {
             std::regex pattern("(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}\%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))");
+            return true;
             return std::regex_match(ipv6, pattern);
         }
 
     public:
-        conn_log(unsigned int c=DEFAULT_CAPACITY)
+        conn_log()
         {
-            // open socket
-            // test socket
+            // open socket                
+            c.conn("localhost", 8673);
+
+            // test connection to Bloomd
+            c.send_data("list\n");
+            std::string reply = c.receive(1024);
+            
+            if (reply.substr(0,5) != "START")
+            {
+                // error
+                std::cout << reply;
+            }
         }
 
         void add_ipv4(std::string mac_address,
@@ -57,11 +76,17 @@ class conn_log
 
             // create timestamp
             time_t timestamp = time(nullptr);
-            // list(timestamp / SUBLOG_LENGTH)
-            // if "Filter does not exist"
-            // then create filter string(timestamp / SUBLOG_LENGTH)
+            
+            // check and create filter based on current timeslot
+            c.send_data("create " + std::to_string(timestamp / SUBLOG_LENGTH) + "\n");
+            std::string reply = c.receive(1024);
+            std::cout << reply;
 
             // set string(timestamp / SUBLOG_LENGTH) string(mac_address + port) 
+            c.send_data("set " + std::to_string(timestamp / SUBLOG_LENGTH) + " "
+                        + mac_address + "|" + std::to_string(port) + "\n");
+            reply = c.receive(1024);
+            std::cout << reply;
         }
 
         bool has_ipv4(std::string mac_address,
@@ -80,7 +105,7 @@ class conn_log
             // throw std::out_of_range("Invalid timestamp - no ipv4 log covering the timestamp's period");
 
             // check string(timestamp / SUBLOG_LENGTH) string(mac_address + port)
-
+            return true;
         }
 /*
         void add_ipv6(std::string mac_address,
--- libs/conn_log/test.cpp
+++ libs/conn_log/test.cpp
@@ -3,6 +3,9 @@
   James H. Loving
 */
 
+#include <iostream>
+#include <string>
+#include <time.h>
 #include <unistd.h>   // for sleep()
 
 #include "conn_log.cpp"
@@ -11,5 +14,15 @@ int main()
 {
     conn_log log;
 
+    std::string mac = "aa:bb:cc:dd:ee:ff";
+
+    log.add_ipv4(mac, 17500);
+
+    /*
+    std::cout << log.has_ipv4(mac,
+                              17500,
+                              time(nullptr));
+    */
+    
     return 0;
 }
