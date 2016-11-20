0e2110a90188ecb369e9b1f658ee584ad407c5c6
jameshloving <jameshloving@gmail.com>
2016-11-20 12:39:54 -0500

    Beautify

--- libs/conn_log/tcp_client.cpp
+++ libs/conn_log/tcp_client.cpp
@@ -38,36 +38,32 @@ tcp_client::tcp_client()
 bool tcp_client::conn(std::string address , int port)
 {
     //create socket if it is not already created
-    if(sock == -1)
+    if (sock == -1)
     {
         //Create socket
         sock = socket(AF_INET , SOCK_STREAM , 0);
         if (sock == -1)
         {
-            perror("Could not create socket");
+            throw std::runtime_error("Could not create socket");
         }
          
-        std::cout<<"Socket created\n";
+        std::cout << "Socket created\n";
     }
-    else    {   /* OK , nothing */  }
      
     //setup address structure
     if(inet_addr(address.c_str()) == -1)
     {
+        //resolve the hostname, since it's not an ip address
+
         struct hostent *he;
         struct in_addr **addr_list;
          
-        //resolve the hostname, its not an ip address
-        if ( (he = gethostbyname( address.c_str() ) ) == NULL)
+        if ((he = gethostbyname(address.c_str())) == NULL)
         {
-            //gethostbyname failed
-            herror("gethostbyname");
-            std::cout<<"Failed to resolve hostname\n";
-             
+            throw std::runtime_error("Gethostbyname failed");
             return false;
         }
          
-        //Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
         addr_list = (struct in_addr **) he->h_addr_list;
  
         for(int i = 0; addr_list[i] != NULL; i++)
@@ -80,37 +76,33 @@ bool tcp_client::conn(std::string address , int port)
             break;
         }
     }
-     
-    //plain ip address
     else
     {
-        server.sin_addr.s_addr = inet_addr( address.c_str() );
+        // address is a plain IP address, so directly assign
+        server.sin_addr.s_addr = inet_addr(address.c_str());
     }
      
     server.sin_family = AF_INET;
-    server.sin_port = htons( port );
+    server.sin_port = htons(port);
      
     //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    if (connect(sock, (struct sockaddr*) &server, sizeof(server)) < 0)
     {
-        perror("connect failed. Error");
-        return 1;
+        throw std::runtime_error("Socket connection failed");
     }
      
-    std::cout<<"Connected\n";
+    std::cout << "Connected\n";
+
     return true;
 }
  
 // Send data to the connected host
 bool tcp_client::send_data(std::string data)
 {
-    //Send some data
-    if( send(sock , data.c_str() , strlen( data.c_str() ) , 0) < 0)
+    if(send(sock, data.c_str(), strlen(data.c_str()), 0) < 0)
     {
-        perror("Send failed : ");
-        return false;
+        throw std::runtime_error("Socket send failed");
     }
-    std::cout<<"Data send\n";
      
     return true;
 }
@@ -121,7 +113,6 @@ std::string tcp_client::receive(int size=512)
     char buffer[size];
     std::string reply;
      
-    //Receive a reply from the server
     if( recv(sock , buffer , sizeof(buffer) , 0) < 0)
     {
         puts("recv failed");
