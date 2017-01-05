373e92b80e9219f04b65a45f1c90d2e4d982dd23
jameshloving <jameshloving@gmail.com>
2017-01-05 15:21:30 -0500

    Break out header, update documentation to Doxygen

--- libs/conn_log/tcp_client.cpp
+++ libs/conn_log/tcp_client.cpp
@@ -1,32 +1,18 @@
-/*
-    tcp_client.cpp
-    James H. Loving
-    modified from http://www.binarytides.com/code-a-simple-socket-client-class-in-c/
-*/
+//=============================================================================
+//
+// Name:        tcp_client.cpp
+// Authors:     James H. Loving
+// Description: This file defines the tcp_client class, used for basic socket
+//              communication. For additional documentation, refer to
+//              tcp_client.hpp.
+//
+//              Note: Modified from http://www.binarytides.com/
+//              code-a-simple-socket-client-class-in-c/
+//
+//=============================================================================
 
-#include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <string>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include "tcp_client.hpp"
 
-class tcp_client
-{
-    private:
-        int sock;
-        std::string address;
-        int port;
-        struct sockaddr_in server;
-         
-    public:
-        tcp_client();
-        bool conn(std::string, int);
-        bool send_data(std::string data);
-        std::string receive(int);
-};
- 
 tcp_client::tcp_client()
 {
     sock = -1;
@@ -34,14 +20,13 @@ tcp_client::tcp_client()
     port = 0;
 }
  
-// Connect to a host on a certain port number
-bool tcp_client::conn(std::string address , int port)
+bool tcp_client::conn(std::string address,
+                      int port)
 {
-    //create socket if it is not already created
+    // create socket if it is not already created
     if (sock == -1)
     {
-        //Create socket
-        sock = socket(AF_INET , SOCK_STREAM , 0);
+        sock = socket(AF_INET, SOCK_STREAM, 0);
         if (sock == -1)
         {
             throw std::runtime_error("Could not create socket");
@@ -50,18 +35,16 @@ bool tcp_client::conn(std::string address , int port)
         std::cout << "Socket created\n";
     }
      
-    //setup address structure
+    // setup address structure
     if(inet_addr(address.c_str()) == -1)
     {
-        //resolve the hostname, since it's not an ip address
-
+        // resolve the hostname, since it's not an ip address
         struct hostent *he;
         struct in_addr **addr_list;
          
         if ((he = gethostbyname(address.c_str())) == NULL)
         {
             throw std::runtime_error("Gethostbyname failed");
-            return false;
         }
          
         addr_list = (struct in_addr **) he->h_addr_list;
@@ -85,7 +68,7 @@ bool tcp_client::conn(std::string address , int port)
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
      
-    //Connect to remote server
+    // connect to remote server
     if (connect(sock, (struct sockaddr*) &server, sizeof(server)) < 0)
     {
         throw std::runtime_error("Socket connection failed");
@@ -96,7 +79,6 @@ bool tcp_client::conn(std::string address , int port)
     return true;
 }
  
-// Send data to the connected host
 bool tcp_client::send_data(std::string data)
 {
     if(send(sock, data.c_str(), strlen(data.c_str()), 0) < 0)
@@ -107,13 +89,19 @@ bool tcp_client::send_data(std::string data)
     return true;
 }
  
-// Receive data from the connected host
+/*!
+    Receive string-encoded data from the server.
+
+    \param size Number of characters to receive.
+
+    \return String-encoded reply.
+*/
 std::string tcp_client::receive(int size=512)
 {
     char buffer[size];
     std::string reply;
      
-    if( recv(sock , buffer , sizeof(buffer) , 0) < 0)
+    if(recv(sock, buffer, sizeof(buffer), 0) < 0)
     {
         puts("recv failed");
     }
---
+++ libs/conn_log/tcp_client.hpp
@@ -0,0 +1,62 @@
+//=============================================================================
+//
+// Name:        tcp_client.hpp
+// Authors:     James H. Loving
+// Description: This file declares the tcp_client class, used for basic socket
+//              communication.
+//
+//              Note: Modified from http://www.binarytides.com/
+//              code-a-simple-socket-client-class-in-c/
+//
+//=============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+class tcp_client
+{
+    private:
+        int sock;                   /*!< network socket */
+        std::string address;        /*!< IPv4 destination address */
+        int port;                   /*!< TCP destination port */
+        struct sockaddr_in server;  /*!< connected server */
+         
+    public:
+        /*!
+            Initialize the TCP client.
+        */
+        tcp_client();
+
+        /*!
+            Connect to a server over IPv4/TCP.
+
+            \param address IPv4 address of server.
+            \param port TCP port on server.
+
+            \return True to indicate successful connection.
+        */
+        bool conn(std::string, int);
+
+        /*!
+            Send string-encoded data to the server.
+
+            \param data Data to send.
+
+            \return True to indicate successful transmission.
+        */
+        bool send_data(std::string data);
+
+        /*!
+            Receive string-encoded data from the server.
+
+            \param size Number of characters to receive.
+
+            \return String-encoded reply.
+        */
+        std::string receive(int);
+};
