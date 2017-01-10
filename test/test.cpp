51883d3eef34b6f7c86835edeec34be4150c1635
jameshloving <jameshloving@gmail.com>
2017-01-10 18:18:07 -0500

    Add MAC & IPv6 validity check functions

--- test/test.cpp
+++ test/test.cpp
@@ -2,16 +2,78 @@
 //
 // Name:        test.cpp
 // Authors:     James H. Loving
-// Description: This file defines all unit tests for edict.
+// Description: This file defines all unit tests for the non-interface
+//              portions of EDICT.
+//
+//              To run manually:
+//              - cmake CMakeLists.txt
+//              - make
+//              - ./runTests
 //
 //=============================================================================
 
-#include "gtest/gtest.h"
+#include <cstdlib>
+#include <string>
+#include <unistd.h>
 
-TEST(TestHierarchy, TestTest)
+#include "gtest/gtest.h"
+#include "../libs/conn_log/conn_log.hpp"
+#include "../libs/device_log/device_log.hpp"
+
+TEST(conn_log, valid_mac)
 {
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 2);
+    system("/home/ubuntu/edict/libs/bloomd/bloomd -f /home/ubuntu/edict/libs/bloomd/bloomd.conf > /dev/null &");
+    sleep(1);
+
+    conn_log c;
+
+    std::string s = "aabbccddeeff";
+    ASSERT_TRUE(c.valid_mac(s));
+
+    s = "001122334455";
+    ASSERT_TRUE(c.valid_mac(s));
+
+    s = "aa11bb22cc33";
+    ASSERT_TRUE(c.valid_mac(s));
+
+    s = "aa:bb:cc:dd:ee:ff";
+    ASSERT_FALSE(c.valid_mac(s));
+
+    s = "/abbccddeeff";
+    ASSERT_FALSE(c.valid_mac(s));
+    
+    s = ":abbccddeeff";
+    ASSERT_FALSE(c.valid_mac(s));
+
+    s = "AABBCCDDEEFF";
+    ASSERT_FALSE(c.valid_mac(s));
+
+    s = "ggbbccddeeff";
+    ASSERT_FALSE(c.valid_mac(s));
+
+    s = "aabbccddeeff\n";
+    ASSERT_FALSE(c.valid_mac(s));
+
+    system("pkill bloomd > /dev/null");
+}
+
+TEST(conn_log, valid_ipv6)
+{
+     system("/home/ubuntu/edict/libs/bloomd/bloomd -f /home/ubuntu/edict/libs/bloomd/bloomd.conf > /dev/null &");
+    sleep(1);
+
+    conn_log c;
+
+    std::string s = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+    ASSERT_TRUE(c.valid_ipv6(s));
+
+    s = "2001:db8:85a3::8a2e:370:7334";
+    ASSERT_TRUE(c.valid_ipv6(s));
+
+    s = "::1::2";
+    ASSERT_FALSE(c.valid_ipv6(s));
+
+    system("pkill bloomd > /dev/null");
 }
 
 int main(int argc, char **argv)
